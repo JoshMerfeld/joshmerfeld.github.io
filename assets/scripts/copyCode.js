b6aedb40833783c6b02537586394244f551b9ea1
Josh Merfeld <74187569+JoshMerfeld@users.noreply.github.com>
2025-03-06 14:01:13 +0900

    Update copyCode.js

--- assets/scripts/copyCode.js
+++ assets/scripts/copyCode.js
@@ -4,25 +4,25 @@ const copyCodeButtons = document.querySelectorAll('.copy-code-button');
 
 copyCodeButtons.forEach((copyCodeButton, index) => {
   const codeBlock = codeBlocks[index];
-  const code = codeBlock.innerText;
-  const languageClass = Array.from(codeBlock.classList).find(cls => cls.startsWith('language-'));
-  const language = languageClass ? languageClass.replace('language-', '') : 'code';
+  const code = codeBlock.querySelector('pre code').innerText;
 
   copyCodeButton.addEventListener('click', () => {
     // Copy the code to the user's clipboard
-    window.navigator.clipboard.writeText(code);
+    window.navigator.clipboard.writeText(code).then(() => {
+      // Update the button text visually
+      const { innerText: originalText } = copyCodeButton;
+      copyCodeButton.innerText = 'Copied!';
 
-    // Update the button text visually
-    const { innerText: originalText } = copyCodeButton;
-    copyCodeButton.innerText = `Copied ${language}!`;
+      // (Optional) Toggle a class for styling the button
+      copyCodeButton.classList.add('copied');
 
-    // (Optional) Toggle a class for styling the button
-    copyCodeButton.classList.add('copied');
-
-    // After 2 seconds, reset the button to its initial UI
-    setTimeout(() => {
+      // After 2 seconds, reset the button to its initial UI
+      setTimeout(() => {
         copyCodeButton.innerText = originalText;
         copyCodeButton.classList.remove('copied');
-    }, 2000);
+      }, 2000);
+    }).catch(err => {
+      console.error('Failed to copy text: ', err);
+    });
   });
 });
