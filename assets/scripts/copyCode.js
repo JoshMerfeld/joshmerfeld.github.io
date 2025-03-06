c17172b626575d2ed42540c3548f13e2ce29ae99
Josh Merfeld <74187569+JoshMerfeld@users.noreply.github.com>
2025-03-06 14:04:42 +0900

    Update copyCode.js

--- assets/scripts/copyCode.js
+++ assets/scripts/copyCode.js
@@ -2,13 +2,27 @@
 const codeBlocks = document.querySelectorAll('.highlighter-rouge');
 const copyCodeButtons = document.querySelectorAll('.copy-code-button');
 
+console.log('Code blocks:', codeBlocks);
+console.log('Copy buttons:', copyCodeButtons);
+
 copyCodeButtons.forEach((copyCodeButton, index) => {
   const codeBlock = codeBlocks[index];
+  if (!codeBlock) {
+    console.error(`No code block found for button at index ${index}`);
+    return;
+  }
   const code = codeBlock.querySelector('pre code').innerText;
 
+  console.log('Code block:', codeBlock);
+  console.log('Code:', code);
+
   copyCodeButton.addEventListener('click', () => {
+    console.log('Button clicked:', copyCodeButton);
+
     // Copy the code to the user's clipboard
     window.navigator.clipboard.writeText(code).then(() => {
+      console.log('Code copied:', code);
+
       // Update the button text visually
       const { innerText: originalText } = copyCodeButton;
       copyCodeButton.innerText = 'Copied!';
